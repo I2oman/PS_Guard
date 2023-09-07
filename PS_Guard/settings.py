b94f402e82f8d613177f62819b4edd5c3631e74a
I2oman <romanhrynchack@gmail.com>
2023-09-07 18:15:45 +0100

    Heroku setup 2

--- PS_Guard/settings.py
+++ PS_Guard/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qeup%9by@fn2--ztuz&6imk(g1^cg#3*z@##7-9v2^)y0&3d=k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('PSG_HOST')]
 
 
 # Application definition
